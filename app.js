2897c4ba9ad6dbd2aeb148dee8dff1296f69620a
sbodrero <bodrero.sebastien@gmail.com>
2020-09-02 19:15:28 +0200

    Put json file content in const and updated dinosList IIFE

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+   const dinosJSON = {"Dinos":[{"species":"Triceratops","weight":13000,"height":114,"diet":"herbavor","where":"North America","when":"Late Cretaceous","fact":"First discovered in 1889 by Othniel Charles Marsh"},{"species":"Tyrannosaurus Rex","weight":11905,"height":144,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"The largest known skull measures in at 5 feet long."},{"species":"Anklyosaurus","weight":10500,"height":55,"diet":"herbavor","where":"North America","when":"Late Cretaceous","fact":"Anklyosaurus survived for approximately 135 million years."},{"species":"Brachiosaurus","weight":70000,"height":"372","diet":"herbavor","where":"North America","when":"Late Jurasic","fact":"An asteroid was named 9954 Brachiosaurus in 1991."},{"species":"Stegosaurus","weight":11600,"height":79,"diet":"herbavor","where":"North America, Europe, Asia","when":"Late Jurasic to Early Cretaceous","fact":"The Stegosaurus had between 17 and 22 seperate places and flat spines."},{"species":"Elasmosaurus","weight":16000,"height":59,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"Elasmosaurus was a marine reptile first discovered in Kansas."},{"species":"Pteranodon","weight":44,"height":20,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"Actually a flying reptile, the Pteranodon is not a dinosaur."},{"species":"Pigeon","weight":0.5,"height":9,"diet":"herbavor","where":"World Wide","when":"Holocene","fact":"All birds are living dinosaurs."}]}
+
     /**
      * @description Represent an Animal
      * @constructor
@@ -115,28 +117,23 @@
     };
 
     /**
-     * @description Fetch a list odf Dinos from json file
+     * @description Fetch a list of Dinos from json object
      * @type {Array} allDinos
      */
     const DinosList = (function() {
         let allDinos = [];
-        fetch('dino.json') // Call the fetch function passing the url of the API as a parameter
-            .then(response => response.json())
-            .then(dinos => {
-                const { Dinos } = dinos;
-                Dinos.map(dino => {
-                    allDinos.push( new Dino(
-                            dino.species,
-                            dino.weight,
-                            dino.height,
-                            dino.diet,
-                            dino.where,
-                            dino.when,
-                            dino.fact
-                        ))
-                    })
-            })
-            .catch(() => { /* TODO print error but no console allowed by rubric */ } );
+        const { Dinos } = dinosJSON;
+        Dinos.map(dino => {
+            allDinos.push( new Dino(
+                    dino.species,
+                    dino.weight,
+                    dino.height,
+                    dino.diet,
+                    dino.where,
+                    dino.when,
+                    dino.fact
+                ))
+            });
         return allDinos;
     })();
 
@@ -232,6 +229,7 @@
             return getEqualSentence('height');
         }
         if(dinoHeight < humanHeight) {
+            diff = humanHeight - dinoHeight;
             return getCompareSentence(diff, 'height', 'less', 'feets');
         }
         diff = dinoHeight - humanHeight;
